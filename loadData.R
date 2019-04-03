60252578bf623ec5a21a60d6633dbf0201b47a62
kristen-sauby <48966037+kristen-sauby@users.noreply.github.com>
2019-04-03 16:26:15 -0400

    set up project

---
+++ loadData.R
@@ -0,0 +1,11 @@
+setwd("C:/Users/Kristen.Sauby/Documents/Projects/Wading_Birds")
+ColonyInfo <- read_excel("ColonyInfo.xlsx")
+ColonyInfo_WMAMerged2April2019 <- 
+		read_excel("ColonyInfo_WildlifeManagementAreasMerged2April2019.xlsx")
+ColonyInfo_NWRMerged2April2019 <- 
+		read_excel("ColonyInfo_NationalWildlifeRefugeMerged2April2019.xlsx")
+ColonyInfo_FLMAMerged2April2019 <- 
+		read_excel("ColonyInfo_FLManagedAreaMerged2April2019.xlsx")
+
+
+
---
+++ prelims.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/Kristen.Sauby/Documents/Projects/Wading_Birds/R_WadingBirds")
+library('ProjectTemplate')
+load.project()
+reload.project()
