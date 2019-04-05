a98cb0ca20da44322a10eff87edd535bcf44be67
kristen-sauby <48966037+kristen-sauby@users.noreply.github.com>
2019-04-05 08:39:57 -0400

    Update createColonyInfo.R

--- code/createColonyInfo.R
+++ code/createColonyInfo.R
@@ -1,7 +1,7 @@
 # get list of ID Numbers NOT in ColonyInfo_FLMAMerged2April2019
 ColonyInfo_remainder <- ColonyInfo %>%
 	filter(!(ColonyInfo$`ID NUM` %in% ColonyInfo_FLMAMerged2April2019$ID_NUM))
-# find those in ColonyInfo_NWRMerged2April2019
+# find those in ColonyInfo_NWRMerged2April2019 and keep
 ColonyInfo_NWRMerged2April2019_keep <- ColonyInfo_NWRMerged2April2019 %>%
 	filter(ColonyInfo_NWRMerged2April2019$ID_NUM %in% ColonyInfo_remainder$`ID NUM`)
 
@@ -13,14 +13,27 @@ ColonyInfo_remainder %<>%
 ColonyInfo_WMAMerged2April2019_keep <- ColonyInfo_WMAMerged2April2019 %>%
 	filter(ColonyInfo_WMAMerged2April2019$ID_NUM %in% ColonyInfo_remainder$`ID NUM`)
 
-# get list of ID Numbers still missing
-ColonyInfo_remainder %<>%
-	filter(!(ColonyInfo_remainder$`ID NUM` %in% ColonyInfo_WMAMerged2April2019$ID_NUM))
 
-write.csv(ColonyInfo_remainder, "ColonyInfo_remainder.csv")
-# add those from original ColonyInfo file
-
-
-ColonyInfo_FLMAMerged2April2019 %>%
+# rbind GIS data
+ColonyInfo_GIS <- ColonyInfo_FLMAMerged2April2019 %>%
 	rbind.fill(ColonyInfo_NWRMerged2April2019_keep) %>%
-	rbind.fill(ColonyInfo_WMAMerged2April2019_keep) %>% write.csv("ColonyInfo_w_GIS_data.csv")
+	rbind.fill(ColonyInfo_WMAMerged2April2019_keep)
+
+# get lat/long to be the same across records
+ColonyInfo_GIS$Lat %<>% round(4)
+ColonyInfo_GIS$Long %<>% round(4)
+
+ColonyInfo$Lat %<>% round(4)
+ColonyInfo$Long %<>% round(4)
+
+# get list of ID Numbers still missing
+ColonyInfo_remainder <- ColonyInfo %>%
+	filter(!(ColonyInfo$`ID NUM` %in% ColonyInfo_GIS$ID_NUM))
+
+# rbind with GIS data
+ColonyInfo_GIS %<>% rbind.fill(ColonyInfo_remainder)
+
+
+
+
+ColonyInfo_GIS %>% write.csv("ColonyInfo_merged.csv")
